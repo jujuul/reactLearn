b1c727186af207fed2aad3fdec4632b2d562fc74
zhangxu <576583430@qq.com>
2021-02-24 17:40:25 +0800

    2021-02-24PM05:40

--- react_staging/05_src_github搜索案例_axios/components/Search/index.jsx
+++ react_staging/05_src_github搜索案例_axios/components/Search/index.jsx
@@ -1,28 +1,23 @@
 import React, { Component } from "react";
-import PubSub from 'pubsub-js'
 import axios from "axios";
 
 export default class Search extends Component {
   search = () => {
-    console.log('search组件发布消息了');
     // 获取用户的输入(连续解构赋值+重命名)
     const {
       keyWordElement: { value: keyWord },
     } = this;
-    // 发送通知前通知List更新状态
-    // this.props.updateAppState({ isFirst: false, isLoading: true })
-    PubSub.publish('atguigu', { isFirst: false, isLoading: true })
+    // 发送通知前通知App更新状态
+    this.props.updateAppState({ isFirst: false, isLoading: true })
     // 发送网络请求
     axios.get(`/api1/search/users?q=${keyWord}`).then(
       (res) => {
-        // 请求成功后通知List更新状态
-        // this.props.updateAppState({ users: res.data.items, isLoading: false });
-        PubSub.publish('atguigu', { users: res.data.items, isLoading: false })
+        // 请求成功后通知App更新状态
+        this.props.updateAppState({ users: res.data.items, isLoading: false });
       },
       (err) => {
-        // 请求失败后通知List更新状态
-        // this.props.updateAppState({ isLoading: false, err:err.message });
-        PubSub.publish('atguigu', { isLoading: false, err: err.message })
+        // 请求成功后通知App更新状态
+        this.props.updateAppState({ isLoading: false, err:err.message });
       }
     );
   };
--- react_staging/src/App.jsx
+++ react_staging/src/App.jsx
@@ -3,16 +3,11 @@ import Search from "./components/Search";
 import List from "./components/List";
 
 export default class App extends Component {
-  state = { users: [] }; // 初始化状态,users初始值为数组
-  saveUsers = (users) => {
-    this.setState({ users });
-  };
   render() {
-    const { users } = this.state;
     return (
       <div className="container">
-        <Search saveUsers={this.saveUsers} />
-        <List users={users} />
+        <Search />
+        <List />
       </div>
     );
   }
