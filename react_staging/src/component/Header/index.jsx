1c7965efa499a8553a9c5b96369c4e2159e4a3ef
jujuul <576583430@qq.com>
2021-03-18 23:13:52 +0800

    2021-03-18PM11:13

--- react_staging/src/component/Header/index.jsx
+++ react_staging/src/component/Header/index.jsx
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 
-export default class Header extends Component {
+class Header extends Component {
+  back = () => {
+    this.props.history.goBack();
+  };
+  forward = () => {
+    this.props.history.goForward();
+  };
   render() {
+    console.log(this.props);
     return (
       <div className="page-header">
         <h2>React Router Demo</h2>
+        <button onClick={this.back}>回退</button>
+        <button onClick={this.forward}>前进</button>
       </div>
     );
   }
 }
+export default withRouter(Header)
